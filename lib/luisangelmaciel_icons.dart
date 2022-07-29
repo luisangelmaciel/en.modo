211d1aae425ae86de1ba12aadeb4dcbdab987601
LuisAngelMaciel <89158021+luisangelmaciel@users.noreply.github.com>
2022-07-29 16:40:56 -0500

    Que vacia?

---
+++ lib/luisangelmaciel_icons.dart
@@ -0,0 +1,61 @@
+// Place fonts/luisangelmaciel.ttf in your fonts/ directory and
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: luisangelmaciel
+//      fonts:
+//       - asset: fonts/luisangelmaciel.ttf
+import 'package:flutter/widgets.dart';
+
+class Luisangelmaciel {
+  Luisangelmaciel._();
+
+  static const String _fontFamily = 'luisangelmaciel';
+
+  static const IconData github_d = IconData(0xe902, fontFamily: _fontFamily);
+  static const IconData github_g = IconData(0xe903, fontFamily: _fontFamily);
+  static const IconData github_x = IconData(0xe904, fontFamily: _fontFamily);
+  static const IconData github = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData hhm = IconData(0xe901, fontFamily: _fontFamily);
+  static const IconData ico_codepen = IconData(0xe92a, fontFamily: _fontFamily);
+  static const IconData ico_codesandbox = IconData(0xe92f, fontFamily: _fontFamily);
+  static const IconData ico_replit = IconData(0xe930, fontFamily: _fontFamily);
+  static const IconData w3 = IconData(0xe931, fontFamily: _fontFamily);
+  static const IconData avatar_hihello = IconData(0xec41, fontFamily: _fontFamily);
+  static const IconData luna_pokemon = IconData(0xea48, fontFamily: _fontFamily);
+  static const IconData sol_pokemon = IconData(0xea4b, fontFamily: _fontFamily);
+  static const IconData mercado_libre = IconData(0xe914, fontFamily: _fontFamily);
+  static const IconData onlyfans = IconData(0xe91b, fontFamily: _fontFamily);
+  static const IconData scruff = IconData(0xe91d, fontFamily: _fontFamily);
+  static const IconData netflix_gayflix = IconData(0xe929, fontFamily: _fontFamily);
+  static const IconData tumblr = IconData(0xe92b, fontFamily: _fontFamily);
+  static const IconData youtube = IconData(0xea5c, fontFamily: _fontFamily);
+  static const IconData tripadvisor = IconData(0xf65e, fontFamily: _fontFamily);
+  static const IconData google = IconData(0xea5b, fontFamily: _fontFamily);
+  static const IconData twitter_bird1 = IconData(0xf5a5, fontFamily: _fontFamily);
+  static const IconData whatsapp = IconData(0xea5a, fontFamily: _fontFamily);
+  static const IconData facebook_azul_letra = IconData(0xf0a1, fontFamily: _fontFamily);
+  static const IconData gmail = IconData(0xe92c, fontFamily: _fontFamily);
+  static const IconData tumbrl = IconData(0xf661, fontFamily: _fontFamily);
+  static const IconData intagram_camera = IconData(0xf66c, fontFamily: _fontFamily);
+  static const IconData pinterest = IconData(0xe92d, fontFamily: _fontFamily);
+  static const IconData send = IconData(0xe92e, fontFamily: _fontFamily);
+  static const IconData repliit = IconData(0xf65a, fontFamily: _fontFamily);
+  static const IconData codesandbox = IconData(0xf65b, fontFamily: _fontFamily);
+  static const IconData w3spaces = IconData(0xf663, fontFamily: _fontFamily);
+  static const IconData hi5 = IconData(0xf662, fontFamily: _fontFamily);
+  static const IconData xiib = IconData(0xf0b5, fontFamily: _fontFamily);
+  static const IconData logo_viajemos_todxs_por_mexico_turismo_gay_turismo_rosa_color = IconData(0xf344, fontFamily: _fontFamily);
+  static const IconData cursor_hand = IconData(0xf630, fontFamily: _fontFamily);
+  static const IconData hospedaje = IconData(0xf63d, fontFamily: _fontFamily);
+  static const IconData codepen = IconData(0xf657, fontFamily: _fontFamily);
+  static const IconData github_logo = IconData(0xf671, fontFamily: _fontFamily);
+  static const IconData www = IconData(0xf65f, fontFamily: _fontFamily);
+  static const IconData twitter_bird = IconData(0xf5a4, fontFamily: _fontFamily);
+  static const IconData flecha_atras = IconData(0xf0a8, fontFamily: _fontFamily);
+  static const IconData flecha_siguiente = IconData(0xf0a9, fontFamily: _fontFamily);
+  static const IconData etiquetas_codigo = IconData(0xf09f, fontFamily: _fontFamily);
+  static const IconData touch_app = IconData(0xf56b, fontFamily: _fontFamily);
+  static const IconData tarjeta_de_regalo = IconData(0xf463, fontFamily: _fontFamily);
+  static const IconData home = IconData(0xf0b3, fontFamily: _fontFamily);
+}
